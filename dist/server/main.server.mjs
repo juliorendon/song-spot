287ad4c41175bc7b23bdef9846b9f6b85f95c755
juliorendon <juliorendon@gmail.com>
2024-06-05 17:42:03 +0200

    cleaning code

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ED5CGBUQ.mjs";import"./chunk-W6RV52TI.mjs";import"./chunk-HQUS2UUF.mjs";export{a as default};
+import{a}from"./chunk-Q6VCPVEO.mjs";import"./chunk-W6RV52TI.mjs";import"./chunk-HQUS2UUF.mjs";export{a as default};
